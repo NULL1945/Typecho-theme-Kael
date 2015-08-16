b30896768d21f82ed5d6ee751e7552c2c7375edb
1995LZH <fjlzh1995@126.com>
2015-08-16 23:13:58 +0800

    Update

--- archive.php
+++ archive.php
@@ -4,13 +4,15 @@
 <main class="content" role="main">
 
     <?php if ($this->have()): ?>
-    <h1 class="post-title"><?php $this->archiveTitle(array(
-            'category'  =>  _t('分类 %s 下的文章'),
-            'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('标签 %s 下的文章'),
-            'author'    =>  _t('%s 发布的文章')
-        ), '', ''); ?>
-    </h1>
+    <article class="post">
+        <h1 class="post-title"><?php $this->archiveTitle(array(
+                'category'  =>  _t('分类 %s 下的文章'),
+                'search'    =>  _t('包含关键字 %s 的文章'),
+                'tag'       =>  _t('标签 %s 下的文章'),
+                'author'    =>  _t('%s 发布的文章')
+            ), '', ''); ?>
+        </h1>
+    </article>
     <?php while($this->next()): ?>
     <article class="post">
         <header class="post-header">
